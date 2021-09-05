45701f00c554fe870c6f1d7b6e090b83e17a7a8a
Feli <felinegri5@gmail.com>
2021-09-05 01:07:26 -0300

    Upload

---
+++ WebServer/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using dnlib.DotNet.Pdb;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    public static class Extensions
+    {
+        public static void SendResult(this HttpListenerResponse response, byte[] buffer)
+        {
+            response.ContentLength64 = buffer.Length;
+            var output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            output.Close();
+        }
+
+        public static async Task<byte[]> ReadBytesFromStreamAsync(this Stream stream)
+        {
+            var buffer = new byte[stream.Length];
+
+            await stream.ReadAsync(buffer, 0, (int)stream.Length);
+
+            return buffer;
+        }
+    }
+}
---
+++ WebServer/WebServer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Net;
+using OpenMod.Core.Plugins;
+using OpenMod.API.Plugins;
+using System.Threading.Tasks;
+using System.IO;
+using OpenMod.Core.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+[assembly: PluginMetadata("Feli.WebServer", 
+    DisplayName = "WebServer",
+    Author = "Feli",
+    Website = "discord.fplugins.com",
+    Description = "A plugin that provides a web server to host files"
+)]
+
+namespace WebServer
+{
+    public class WebServer : OpenModUniversalPlugin
+    {
+        private readonly ILogger<WebServer> logger;
+        private readonly IConfiguration configuration;
+
+        private HttpListener httpListener;
+
+        private string directory;
+
+        public WebServer(
+            ILogger<WebServer> logger,
+            IConfiguration configuration,
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+            httpListener = new HttpListener();
+            directory = Path.Combine(WorkingDirectory, "WebServerFiles");
+        }
+
+        protected override async Task OnLoadAsync()
+        {
+            File.Delete(Path.Combine(WorkingDirectory, "WebServerFiles.404.html"));
+            File.Delete(Path.Combine(WorkingDirectory, "WebServerFiles.index.html"));
+            File.Delete(Path.Combine(WorkingDirectory, "WebServerFiles.1011.png"));
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+
+            var index = GetType().Assembly.GetManifestResourceStream("WebServer.WebServerFiles.index.html");
+            var notFound = GetType().Assembly.GetManifestResourceStream("WebServer.WebServerFiles.404.html");
+            var testImage = GetType().Assembly.GetManifestResourceStream("WebServer.WebServerFiles.1011.png");
+
+            if(configuration.GetSection("exampleFiles").Get<bool>())
+            {
+                File.WriteAllBytes(Path.Combine(directory, "index.html"), await index.ReadBytesFromStreamAsync());
+                File.WriteAllBytes(Path.Combine(directory, "404.html"), await notFound.ReadBytesFromStreamAsync());
+                File.WriteAllBytes(Path.Combine(directory, "1011.png"), await testImage.ReadBytesFromStreamAsync());
+            }
+            
+
+            AsyncHelper.Schedule("WebServer", () => RunWebServer());
+        }
+
+        private Task RunWebServer()
+        {
+            logger.LogInformation("Starting Web Server");
+
+            httpListener.Prefixes.Add(configuration["webServer:serverAddress"]);
+            httpListener.Start();
+
+            logger.LogInformation("Web Server started");
+
+            while (IsComponentAlive && httpListener.IsListening)
+            {
+                logger.LogInformation("Listening...");
+                var result = httpListener.BeginGetContext(HandleRequest, httpListener);
+                result.AsyncWaitHandle.WaitOne();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private void HandleRequest(IAsyncResult result)
+        {
+            if (!httpListener.IsListening)
+                return;
+
+            var listener = (HttpListener)result.AsyncState;
+
+            var context = listener.EndGetContext(result);
+            logger.LogInformation("Handling request from: {RemoteEndPoint} to: {RawUrl}", context.Request.RemoteEndPoint, context.Request.RawUrl);
+            var response = context.Response;
+            
+            var split = context.Request.RawUrl.Split('/').ToList();
+
+            var searchFile = split.LastOrDefault();
+            split.Remove(searchFile);
+
+            var path = Path.Combine(split.ToArray());
+            path = Path.Combine(directory, path);
+
+            var dir = new DirectoryInfo(path);
+
+            var notFound = Path.Combine(directory, "404.html");
+            
+            if (!dir.Exists)
+            {
+                var rawData = File.ReadAllBytes(notFound);
+
+                response.SendResult(rawData);
+                return;
+            }
+
+            var file = dir.GetFiles().FirstOrDefault(x =>
+            {
+                var split = x.Name.Split('.').ToList();
+                split.RemoveAt(split.Count - 1);
+
+                var fileName = string.Join(".", split);
+
+                return fileName == searchFile || x.Name == searchFile;
+            });
+
+            if (file == null || !file.Exists)
+            {
+                if (dir.GetFiles().FirstOrDefault(x => x.Name == "index.html") != null)
+                {
+                    file = new FileInfo(dir.GetFiles().FirstOrDefault(x => x.Name == "index.html").FullName);
+                }
+                else
+                {
+                    file = new FileInfo(notFound);
+                }
+            }
+
+            var buffer = File.ReadAllBytes(file.FullName);
+
+            response.SendResult(buffer);
+        }
+
+        protected override Task OnUnloadAsync()
+        {
+            if (httpListener.IsListening)
+            {
+                httpListener.Stop();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
